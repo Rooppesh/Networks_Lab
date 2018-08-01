f9b6d15fe77c5a896a83411bb8c8f74bfdf0e95a
Rooppesh Sarankapani <rooppesh.sarankapani@gmail.com>
2018-08-01 14:56:44 +0530

    Add files via upload

---
+++ Lab3/strclient.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;  
+public class strclient{  
+public static void main(String[] args) throws Exception {  
+DatagramSocket ds = new DatagramSocket();  
+Scanner var=new Scanner(System.in);
+String str=var.nextLine();
+InetAddress ip = InetAddress.getByName("127.0.0.1");  
+DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 3000);  
+ds.send(dp);  
+ds.close();  
+}  
+}  
